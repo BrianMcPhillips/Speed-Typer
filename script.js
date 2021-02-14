2f6a2de895925a71a3960bc7cc0beaae39e87221
Brian McPhillips <B.McPhillips89@gmail.com>
2021-02-13 21:44:32 -0800

    Import DOM elements, initialize state, getRandomWord function

---
+++ script.js
@@ -0,0 +1,36 @@
+// Import DOM elements
+const word = document.getElementById('word'),
+  text = document.getElementById('text'),
+  scoreEl = document.getElementById('score'),
+  timeEl = document.getElementById('time'),
+  endgameEl = document.getElementById('end-game'),
+  settingsBtn = document.getElementById('settings-btn'),
+  settings = document.getElementById('settings'),
+  settingsForm = document.getElementById('settings-form'),
+  difficultySelect = document.getElementById('difficulty');
+
+// List of words for game
+const words = [
+  'sense', 'planes', 'position',
+  'circle', 'vase', 'nation',
+  'fruit', 'advertisement', 'cat',
+  'harbor', 'cakes', 'use',
+  'brother', 'bike', 'example',
+  'playground', 'lettuce', 'smile',
+  'summer', 'dress', 'vest',
+  'nest', 'protest', 'deer',
+  'rail', 'suit', 'look', 'hour',
+  'plants', 'stretch', 'governer',
+  'sail', 'hope', 'hair', 'design',
+  'dirt', 'field', 'button'
+];
+
+// Initialize State
+let randomWord;
+let score = 0;
+let time = 10;
+
+// Get random word
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
